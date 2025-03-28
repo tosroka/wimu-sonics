3a61a4734f0d899185802f5fcdfd81a567d732f1
Tomasz S <01169239@pw.edu.pl>
2025-03-28 12:26:09 +0100

    fix: skip nonexisting videos, remove broken requirement

--- scripts/yt_download.py
+++ scripts/yt_download.py
@@ -1,11 +1,16 @@
 from yt_dlp import YoutubeDL
+from yt_dlp.utils import DownloadError
 
 
 def download_audio(filename: str, yt_id: str):
     url = f'https://www.youtube.com/watch?v={yt_id}'
-    with YoutubeDL({'format': 'bestaudio', 'outtmpl': f'{filename}.mp3'}) as video:
-        video.download(url)
+    try:
+        with YoutubeDL({'format': 'bestaudio', 'outtmpl': f'{filename}.mp3'}) as video:
+            video.download(url)
+    except DownloadError as e:
+        print(f"{yt_id} not available")
+
 
 
 if __name__ == "__main__":
-    download_audio('real_00001', 'gl1aHhXnN1k')
+    download_audio('real_00001', 'gl1aahXnN1k')
