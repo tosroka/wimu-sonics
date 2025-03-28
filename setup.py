b49546c8314dd173046c4db74916df85925949ea
Tomasz S <01169239@pw.edu.pl>
2025-03-28 12:26:09 +0100

    fix: skip nonexisting videos, remove broken requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wimu-sonics',
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
     packages=find_packages('src'),
     version='0.1.0',
     description='A short description of the project.',
