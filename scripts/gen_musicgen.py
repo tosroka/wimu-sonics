4f3bd417087589109119c6e21ab6c3e03b23f916
Dominik <domik141202@gmail.com>
2025-05-26 00:12:32 +0200

    add auto generate for musicgen

---
+++ scripts/gen_musicgen.py
@@ -0,0 +1,35 @@
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
+
+import wimu_sonics.data.load_data as wimu
+from argparse import ArgumentParser
+import os
+
+musicgen_in = wimu.get_musicgen()
+musicgen_out = wimu.get_data_dir() / 'examples' / 'musicgen'
+
+def read_prompts(start_idx):
+    prompts = []
+    for file in os.listdir(musicgen_in):
+        number = int(file.split('.')[0])
+        if number < start_idx:
+            continue
+        with open(musicgen_in / file, 'r') as f:
+            prompts.append(f.read())
+    return prompts
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("n", type=int, help="First prompt number from which musicgen starts generate music")
+    args = parser.parse_args()
+
+    prompts = read_prompts(args.n)
+
+    model = MusicGen.get_pretrained('facebook/musicgen-melody')
+    model.set_generation_params(duration=100)
+
+    waves = model.generate(prompts)
+
+    for idx, wav in enumerate(waves):
+        audio_write(musicgen_out / f'{idx}', wav.cpu(), model.sample_rate, strategy="loudness", loudness_compressor=True)
