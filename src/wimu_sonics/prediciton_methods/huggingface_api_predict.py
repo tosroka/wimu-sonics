6ef9dbb394072965e6b0984af8b1b26a89685e01
Tomasz S <01169239@pw.edu.pl>
2025-06-02 16:45:00 +0200

    feat: remove or move around files

--- src/wimu_sonics/prediciton_methods/huggingface_api_predict.py
+++ src/wimu_sonics/prediciton_methods/huggingface_api_predict.py
@@ -1,6 +1,23 @@
 from gradio_client import Client, handle_file
+import numpy as np
 
 client = None
+
+def get_middle_chunk(audio):
+    """This function would reproduce api responses if exact match is needed.
+    Locally we pick different part of the waveform."""
+    chunk_samples = int(120 * 16000)
+    total_chunks = len(audio) // chunk_samples
+    middle_chunk_idx = total_chunks // 2
+
+    # Extract middle chunk
+    start = middle_chunk_idx * chunk_samples
+    end = start + chunk_samples
+    chunk = audio[start:end]
+
+    if len(chunk) < chunk_samples:
+        chunk = np.pad(chunk, (0, chunk_samples - len(chunk)))
+    return chunk
 
 def get_predictions_hf(audio_files): 
     global client # again not pretty
